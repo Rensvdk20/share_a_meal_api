579a9c6364d56794b9687f336db1ae5bd637453a
Rensvdk20 <rensvdk@gmail.com>
2022-04-24 21:48:55 +0200

    Added the user routes of the api

--- index.js
+++ index.js
@@ -1,75 +1,154 @@
-//Deploy heroku
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser");
+const { get } = require("express/lib/response");
 app.use(bodyParser.json());
 
 let database = [];
-let id = 0;
+let user_id = 0;
 
 app.all("*", (req, res, next) => {
     const method = req.method;
-    console.log(`Method ${method} is aangeroepen`);
+    console.log(`Method ${method} has been called`);
     next();
 });
 
 app.get("/", (req, res) => {
     res.status(200).json({
         status: 200,
-        result: "Hello World",
+        result: "Share A Meal API",
     });
 });
 
-app.post("/api/movie", (req, res) => {
-    let movie = req.body;
-        id++;
-        movie = {
-        id,
-        ...movie,
-    };
+//########## Users ###############
 
-    console.log(movie);
+//Register user
+app.route('/api/user')
+.post((req, res) => {
+    let user = req.body;
+    user_id++;
+
+    if(!checkEmailDuplicate(user.emailAdress)) {
+        user = {
+            id: user_id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            street: user.street,
+            password: user.password,
+            emailAdress: user.emailAdress
+        };
     
-    database.push(movie);
-    res.status(201).json({
-        status: 201,
-        result: database,
-    });
-});
-
-app.get("/api/movie/:movieId", (req, res, next) => {
-    const movieId = req.params.movieId;
-    console.log(`Movie met ID ${movieId} gezocht`);
-    let movie = database.filter((item) => item.id == movieId);
-    if (movie.length > 0) {
-        console.log(movie);
-        res.status(200).json({
-            status: 200,
-            result: movie,
+        database.push(user);
+        res.status(201).json({
+            status: 201,
+            result: user
         });
     } else {
-        res.status(401).json({
-            status: 401,
-            result: `Movie with ID ${movieId} not found`,
+        res.status(409).json({
+            status: 409,
+            result: "Email already exists",
+        });
+    }
+})
+
+//Get all users
+.get((req, res) => {
+    res.status(200).json({
+        status: 200,
+        result: database
+    });
+});
+
+//Request current user profile
+app.get("/api/user/profile", (req, res) => {
+    res.status(501).json({
+        status: 501,
+        result: "This endpoint is not yet implemented"
+    });
+});
+
+//Get user by id
+app.route('/api/user/:id')
+.get((req, res) => {
+    const userId = req.params.id;
+    let user = database.find((item) => item.id == userId);
+    if (user) {
+        res.status(200).json({
+            status: 200,
+            result: user,
+        });
+    } else {
+        res.status(404).json({
+            status: 404,
+            result: "User not found"
+        });
+    }
+})
+
+//Update user
+.put((req, res) => {
+    let newUserInfo = req.body;
+    const userId = req.params.id;
+    let userIndex = database.findIndex((obj => obj.id == userId));
+
+    if(userIndex > -1) {
+        database[userIndex] = {
+            id: parseInt(userId),
+            firstName: newUserInfo.firstName,
+            lastName: newUserInfo.lastName,
+            street: newUserInfo.street,
+            city: newUserInfo.city,
+            password: newUserInfo.password,
+            emailAdress: newUserInfo.emailAdress
+        };
+
+        res.status(200).json({
+            status: 200,
+            result: database[userIndex]
+        });
+    } else {
+        res.status(404).json({
+            status: 404,
+            result: "User not found"
+        });
+    }
+})
+
+//Delete user
+.delete((req, res) => {
+    const userId = req.params.id;
+    let userIndex = database.findIndex((obj => obj.id == userId));
+    if(userIndex > -1) {
+        database.splice(userIndex, 1);
+
+        res.status(202).json({
+            status: 202,
+            result: "Success"
+        });
+    } else {
+        res.status(404).json({
+            status: 404,
+            result: "User not found"
         });
     }
 });
 
-app.get("/api/movie", (req, res, next) => {
-    res.status(200).json({
-        status: 200,
-        result: database,
-    });
-});
-
+//End-point not found
 app.all("*", (req, res) => {
     res.status(401).json({
         status: 401,
         result: "End-point not found",
     });
 });
+
+function checkEmailDuplicate(emailAdress) {
+    const filteredArray = database.filter((o) => o.emailAdress === emailAdress);
+    if(filteredArray.length > 0) {
+        return true;
+    } return false;
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
