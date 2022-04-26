b4094159b1458b07779b7e670d09185ecbc5c101
Rensvdk20 <rensvdk@gmail.com>
2022-04-26 15:36:05 +0200

    Merge feature-testing with main

---
+++ test/integration/user.controller.test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../index');
+let database = [];
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('Manage users api/user', () => {
+    describe('UC-201 add movies', () => {
+        beforeEach((done) => {
+            database = [];
+            done();
+        });
+
+        it('When a required input is missing, a valid error should be returned', (done) => {
+            chai.request(server).post('/api/user').send({
+                //Firstname is missing
+                lastName: "Doe",
+                street: "Lovensdijkstraat 61",
+                city: "Breda",
+                isActive: true,
+                emailAdress: "j.doe@server.com",
+                phoneNumber: "+31612345678",
+                password: "secret"
+            })
+            .end((err, res) => {
+                res.should.be.an('object');
+                let { status, result } = res.body;
+                status.should.equal(400);
+                result.should.be.a('string').that.equals('Firstname must be a string');
+                done();
+            });
+        });
+    })
+});
